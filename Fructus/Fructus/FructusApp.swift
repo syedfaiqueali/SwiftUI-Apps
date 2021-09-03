2d05d5721fa57ebe9bc0f93e0c887a1ae802e47d
Faiq Ali <faiqueali017@gmail.com>
2021-09-03 20:42:46 +0500

    Adding assets to the project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Faiq on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
