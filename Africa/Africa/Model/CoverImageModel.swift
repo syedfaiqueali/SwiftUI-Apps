4f21758e39fc9635a11b6f33757193e29adb1ef0
Faiq Ali <faiqueali017@gmail.com>
2021-09-05 17:02:27 +0500

    Decode a json file and fetch the data

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Faiq on 05/09/2021.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
