1e73a9eff551d8f95ba7d9d2e42f92f9e741de82
Faiq Ali <faiqueali017@gmail.com>
2021-09-07 23:05:18 +0500

    Create a video player list

--- Africa/Africa/App/VideoListView.swift
+++ Africa/Africa/App/VideoListView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK:- PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    //MARK:- BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle()) //for background color
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        //Shuffle videos and add haptics
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION
     }
 }
 
+//MARK:- PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
