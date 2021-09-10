c038993022843495f8f730f75b0350c123d6c8ef
Faiq Ali <faiqueali017@gmail.com>
2021-09-10 22:11:35 +0500

    Created motion animation programmaticallly

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct GalleryView: View {
     var body: some View {
-        Text("GalleryView")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("GalleryView")
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
