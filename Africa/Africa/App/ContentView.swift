1e81cdd00b54279a1ec8e6f857c68c702fb10e52
Faiq Ali <faiqueali017@gmail.com>
2021-09-12 22:07:32 +0500

    Africa completed

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -54,6 +54,9 @@ struct ContentView: View {
                                 AnimalListItemView(animal: animal)
                             } //: LINK
                         } //: LOOP
+                        
+                        CreditsView()
+                            .modifier(CenterModifier())
                     } //: LIST
                 } else {
                     ScrollView(.vertical, showsIndicators: false) {
---
+++ Africa/Africa/View/CreditsView.swift
@@ -0,0 +1,36 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Faiq on 12/09/2021.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        VStack {
+            Image("compass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 128, height: 128)
+            Text("""
+    Copyright © Faique Ali
+    All rights reserved
+    Better Apps ♡ less code
+    """)
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+        }
+        .padding()
+        .opacity(0.4)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
