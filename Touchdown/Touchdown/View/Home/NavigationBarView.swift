14ab082badd9f2725f49aebd320c6cd25c3c05cc
Faiq Ali <faiqueali017@gmail.com>
2021-09-23 22:04:51 +0500

    Custom Navbar with logo

---
+++ Touchdown/Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,61 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Faiq on 23/09/2021.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK:- PROPERTIES
+    @State private var isAnimated: Bool = false
+    
+    // MARK:- BODY
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }) //: BUTTON
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                    
+                }
+            }) //: BUTTON
+            
+        } //: HSTACK
+    }
+}
+
+// MARK:- PREVIEW
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
