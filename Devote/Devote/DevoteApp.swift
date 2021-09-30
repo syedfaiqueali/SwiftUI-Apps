d1461a9afc6094cda179fbf4a06c03fe28230587
Faiq Ali <faiqueali017@gmail.com>
2021-09-30 21:41:26 +0500

    Project setup

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Faiq on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
