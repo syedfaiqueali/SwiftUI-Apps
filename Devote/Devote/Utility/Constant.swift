a28e90e8aa85c96a34e6caaa479176caf533fe05
Faiq Ali <faiqueali017@gmail.com>
2021-10-01 11:00:45 +0500

    Code organizing

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Faiq on 01/10/2021.
+//
+
+import SwiftUI
+
+// MARK:- FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+// MARK:- UI
+
+// MARK:- UX
