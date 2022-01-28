3f72711755680fc830fa5704c9d4d46a31db33e0
faiqueali017 <faiqueali017@gmail.com>
2022-01-28 16:04:10 +0500

    Update Excercise1_The_Rotation.swift

--- Animations/Animations/Views/Excercise_1/Excercise1_The_Rotation.swift
+++ Animations/Animations/Views/Excercise_1/Excercise1_The_Rotation.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct Excercise1_The_Rotation: View {
     @State private var change = false
+    var offsetValue: CGFloat = 200
     
     var body: some View {
         VStack(spacing: 20) {
             TitleText("Excercise")
-            SubtitleText("Rotate with Rotation Effect")
-            BannerText("Add a rotation modifier and change the degrees. It rotates depending on the change in variable", .white, .red)
+            SubtitleText("Move Shapes with offsets")
+            BannerText("Need to move each shape with rotation effect when the change occurs", .white, .red)
             
             Spacer()
             
@@ -22,15 +23,19 @@ struct Excercise1_The_Rotation: View {
                 HStack(alignment: .bottom, spacing: 1){
                     Rectangle()
                         .frame(width:70, height:35)
+                        .offset(x: change ? 0 : -offsetValue) //Move left
                     Rectangle()
                         .frame(width:35, height:70)
+                        .offset(y: change ? 0 : -offsetValue) // Move up
                 }
                 .offset(x: -18)
                 HStack(alignment: .top, spacing: 1){
                     Rectangle()
                         .frame(width:36, height:72)
+                        .offset(y: change ? 0 : offsetValue) //Move down
                     Rectangle()
                         .frame(width:72, height:36)
+                        .offset(x: change ? 0 : offsetValue) //Move right
                 }
                 .offset(x: 18)
             }
