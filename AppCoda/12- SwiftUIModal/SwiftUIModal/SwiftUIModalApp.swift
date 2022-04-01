ace06b4cd4963f69cf0ee621e30c8a041d3f53c5
Faique Ali <faiqueali017@gmail.com>
2022-04-01 12:12:43 +0500

    Sheet and full screen model navigations

---
+++ AppCoda/12- SwiftUIModal/SwiftUIModal/SwiftUIModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModalApp.swift
+//  SwiftUIModal
+//
+//  Created by Simon Ng on 19/8/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
